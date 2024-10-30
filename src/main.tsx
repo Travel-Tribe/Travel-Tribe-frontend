9a020a2dd20cbb2d103ee6d326f69738683241e7
Seungmani <dkelcncjs@naver.com>
2024-10-30 13:57:28 +0900

    [eat] MockData 연동

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+async function deferRender() {
+  if (process.env.NODE_ENV !== "development") return;
+
+  const { worker } = await import("./mocks/browser");
+
+  return worker.start({
+    onUnhandledRequest: "bypass",
+  });
+}
+
+deferRender().then(() => {
+  createRoot(document.getElementById("root")!).render(<App />);
+});
