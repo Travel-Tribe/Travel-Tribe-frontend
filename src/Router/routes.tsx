10fe9985599ab7a0c3be5d9bec863e15bfcdf3c0
joo93 <joo93d.o@gmail.com>
2024-11-02 17:16:54 +0900

    [feat] : 라우트 구조에 인증 및 프로필 검증 추가

--- src/Router/routes.tsx
+++ src/Router/routes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import React from "react";
 import { Suspense } from "react";
 import ProfileEdit from "../Components/Mypage/ProfileEdit";
@@ -6,7 +6,10 @@ import MyProfileMain from "../Components/Mypage/MyProfileMain";
 import MyCompletedTrips from "../Components/Mypage/MyCompletedTrips";
 import MyTravelHistory from "../Components/Mypage/MyTravelHistory";
 import MyAccountSettings from "../Components/Mypage/MyAccountSettings";
-import Header from "../Components/Common/Header";
+import PrivateRoutes from "../Components/Layout/PrivateRoutes";
+import PublicOnlyRoutes from "../Components/Layout/PublicOnlyRoutes";
+import Layout from "../Components/Layout";
+import { ProfileRequiredRoutes } from "../Components/Layout/ProfileRequiredRoutes";
 
 const Error = React.lazy(() => import("../Page/Error"));
 const SignIn = React.lazy(() => import("../Page/SignIn"));
@@ -21,40 +24,46 @@ const ReviewPost = React.lazy(() => import("../Page/ReviewPost"));
 const MyPage = React.lazy(() => import("../Page/MyPage"));
 
 const Router = (): JSX.Element => {
-  const location = useLocation();
-  const hideHeaderRoutes = ["/signIn", "/signUp"];
-
   return (
     <Suspense fallback={""}>
-      {!hideHeaderRoutes.includes(location.pathname) && <Header />}
-
       <Routes>
-        <Route path="*" element={<Error />} />
-        <Route path="/" element={<Navigate to="/recruitment" replace />} />
-
-        {/* 모집 관련 라우트 */}
-        <Route path="/recruitment" element={<Recruitment />}>
-          <Route path="write" element={<RecruitForm />} />
-          <Route path="edit/:id" element={<RecruitForm />} />
-          <Route path=":id" element={<RecruitPost />} />
+        {/* 공개 전용 라우트 - 로그인하지 않은 사용자만 접근 가능 */}
+        <Route element={<PublicOnlyRoutes />}>
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/find" element={<Find />} />
         </Route>
 
-        <Route path="/review" element={<Review />}>
-          <Route path="write" element={<ReviewForm />} />
-          <Route path="edit/:id" element={<ReviewForm />} />
-          <Route path=":id" element={<ReviewPost />} />
-        </Route>
+        {/* Layout으로 감싸진 모든 페이지 */}
+        <Route element={<Layout />}>
+          {/* 로그인 필요한 라우트 */}
+          <Route element={<PrivateRoutes />}>
+            {/* 프로필 필수 라우트 */}
+            <Route element={<ProfileRequiredRoutes />}>
+              {/* 글쓰기 관련 */}
+              <Route path="/recruitment/write" element={<RecruitForm />} />
+              <Route path="/recruitment/edit/:id" element={<RecruitForm />} />
+              <Route path="/review/write" element={<ReviewForm />} />
+              <Route path="/review/edit/:id" element={<ReviewForm />} />
+            </Route>
 
-        <Route path="/signIn" element={<SignIn />} />
-        <Route path="/signUp" element={<SignUp />} />
-        <Route path="/find" element={<Find />} />
+            {/* 프로필 불필요 라우트 */}
+            <Route path="/mypage" element={<MyPage />}>
+              <Route index element={<MyProfileMain />} />
+              <Route path="myProfileEdit" element={<ProfileEdit />} />
+              <Route path="completedTrips" element={<MyCompletedTrips />} />
+              <Route path="travelHistory" element={<MyTravelHistory />} />
+              <Route path="accountSettings" element={<MyAccountSettings />} />
+            </Route>
+          </Route>
 
-        <Route path="/mypage" element={<MyPage />}>
-          <Route index element={<MyProfileMain />} />
-          <Route path="myProfileEdit" element={<ProfileEdit />} />
-          <Route path="completedTrips" element={<MyCompletedTrips />} />
-          <Route path="travelHistory" element={<MyTravelHistory />} />
-          <Route path="accountSettings" element={<MyAccountSettings />} />
+          {/* 로그인 필요없는 일반 페이지 */}
+          <Route path="/" element={<Navigate to="/recruitment" replace />} />
+          <Route path="/recruitment" element={<Recruitment />} />
+          <Route path="/recruitment/:id" element={<RecruitPost />} />
+          <Route path="/review" element={<Review />} />
+          <Route path="/review/:id" element={<ReviewPost />} />
+          <Route path="*" element={<Error />} />
         </Route>
       </Routes>
     </Suspense>
