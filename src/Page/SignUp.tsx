c62651075c583c2a9120cd1474f14759c84112b3
joo93 <joo93d.o@gmail.com>
2024-11-04 14:41:39 +0900

    [feat] : 로고 추가 및 홈 링크 연결

--- src/Page/SignUp.tsx
+++ src/Page/SignUp.tsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
 import SignUpForm from "../Components/SignUpForm";
 
 const SignUp = (): JSX.Element => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-custom-bg">
+    <div className="min-h-screen flex items-center justify-center bg-custom-bg flex-col">
+      <h1 className="text-4xl font-bold mb-8">
+        <Link to={"/"}>여행족</Link>
+      </h1>
       <div className="card w-full max-w-md bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className="card-title text-2xl font-bold justify-center">
