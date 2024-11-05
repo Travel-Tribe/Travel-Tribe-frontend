57ba37bc5e0e78ba19364f2fc59de606a1a73108
Seungmani <dkelcncjs@naver.com>
2024-11-05 21:48:44 +0900

    [feat] MBTI 선택 창 및 글 작성 버튼 생성

---
+++ src/Constants/MBTI.ts
@@ -0,0 +1,18 @@
+export const MBTI = Object.freeze([
+  "ISTJ",
+  "ISFJ",
+  "INFJ",
+  "INTJ",
+  "ISTP",
+  "ISFP",
+  "INFP",
+  "INTP",
+  "ESTP",
+  "ESFP",
+  "ENFP",
+  "ENTP",
+  "ESTJ",
+  "ESFJ",
+  "ENFJ",
+  "ENTJ",
+]);
