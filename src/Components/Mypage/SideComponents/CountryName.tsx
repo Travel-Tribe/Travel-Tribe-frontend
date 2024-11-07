d19b55812120e19202d7389f1ac473c3e96ebd03
momo <rhwoah7190@gmail.com>
2024-11-07 23:54:53 +0900

    [refactor] MyLang, CountryName 컴포넌트 사용

--- src/Components/Mypage/SideComponents/CountryName.tsx
+++ src/Components/Mypage/SideComponents/CountryName.tsx
@@ -7,11 +7,11 @@ const CountryName = ({ countries = [] }: MyCountrys): JSX.Element => {
     <div className="mb-8">
       <h4 className="text-lg font-bold">다녀온 국가</h4>
       {countries.length > 0 && (
-        <div className="flex mt-2">
+        <div className="flex">
           {countries.map((country, index) => (
             <div
               key={index}
-              className="w-16 h-5 text-sm rounded-lg bg-custom-pink text-white text-center m-2.5 flex items-center justify-center"
+              className="h-6 badge text-sm rounded-lg border border-black bg-white text-center m-2.5 flex items-center justify-center"
             >
               {country}
             </div>
