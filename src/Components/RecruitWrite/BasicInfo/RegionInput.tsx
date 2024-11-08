474158e2020a75c931bd01fff71d116cdea5ddd4
Seungmani <dkelcncjs@naver.com>
2024-11-08 14:51:01 +0900

    [fix] MultiInput library로 수정

--- src/Components/RecruitWrite/BasicInfo/RegionInput.tsx
+++ src/Components/RecruitWrite/BasicInfo/RegionInput.tsx
@@ -1,30 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTravelData } from "../../../Hooks/useTravelData";
-import MultipleValueTextInput from "../../Common/MultipleValueTextInput";
+import CreatableSelect from "react-select/creatable";
 
 const RegionInput = React.memo((): JSX.Element => {
+  const [cities, setCities] = useState<string[]>([]);
   const { updateTravelData } = useTravelData();
 
-  const handleOnItemAdded = (newItem: string, resultItems: string[]) => {
-    updateTravelData("region", resultItems.join(" "));
-  };
-
-  const handleOnItemDeleted = (deletedItem: string, resultItems: string[]) => {
-    updateTravelData("region", resultItems.join(","));
+  const handleOnItemAdded = (newValue: any) => {
+    const newCity = newValue.map(item => item.value);
+    setCities(newCity);
+    updateTravelData("region", newCity.join(" "));
   };
 
   return (
     <div className="flex items-center mb-2">
       <p className="text-[18px] mr-2">도시:</p>
-      <MultipleValueTextInput
-        className="w-[350px]"
-        onItemAdded={(item, allItems) => {
-          handleOnItemAdded(item, allItems);
-        }}
-        onItemDeleted={(item, allItems) => handleOnItemDeleted(item, allItems)}
-        label="Items"
-        name="item-input"
+      <CreatableSelect
+        isMulti
+        value={cities.map(city => ({
+          label: city,
+          value: city,
+        }))}
+        onChange={handleOnItemAdded}
         placeholder="enter키를 통해 연속입력이 가능합니다."
+        className="w-[350px]"
       />
     </div>
   );
