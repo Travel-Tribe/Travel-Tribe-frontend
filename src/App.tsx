40f5a08c90a1a94cde8cd1d3fb57dccba2cf6c0c
Seungmani <dkelcncjs@naver.com>
2024-11-13 16:58:12 +0900

    [feat] 모집 글 목록 페이지 필터링 및 intersection Observer 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false, //// 창이 포커스될 때 자동으로 리페치하지 않음
+      refetchOnWindowFocus: false, // 창이 포커스될 때 자동으로 리페치하지 않음
       retry: 0, // 실패시 재시도 안함
     },
   },
