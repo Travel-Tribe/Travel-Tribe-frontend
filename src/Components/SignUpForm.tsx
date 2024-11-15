384629c8b456f111a1341ef2169dcbca02921554
momo <rhwoah7190@gmail.com>
2024-11-16 02:26:04 +0900

    [fix] 서버 연동 테스트 후 코드 수정

--- src/Components/SignUpForm.tsx
+++ src/Components/SignUpForm.tsx
@@ -175,6 +175,7 @@ const SignUp = (): JSX.Element => {
       );
 
       console.log("회원가입", response);
+
       if (response.data.result === "SUCCESS") {
         alert("회원가입이 완료되었습니다.");
         navigate("/signIn"); // 로그인 페이지로 이동
