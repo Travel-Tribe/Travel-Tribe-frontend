77b17276a2f4dd11d807e1ac4f3819739f245180
Seungmani <dkelcncjs@naver.com>
2024-11-21 14:32:30 +0900

    [fix] 모집 글 필터링 params 수정 및 국가-대륙 코드, 이름 변경

--- src/Page/Review.tsx
+++ src/Page/Review.tsx
@@ -43,7 +43,7 @@ const Review = React.memo(
         if (value) params.append(key, value);
       });
 
-      return params.toString();
+      return "&" + params.toString();
     };
 
     const fetchRecruitData = async ({ pageParam = 0 }) => {
@@ -70,12 +70,10 @@ const Review = React.memo(
     } = useInfiniteQuery({
       queryKey: [
         "reviewData",
-        {
-          search,
-          city,
-          continent: selectedContinent,
-          country: selectedCountry,
-        },
+        search,
+        city,
+        selectedContinent,
+        selectedCountry,
       ],
       queryFn: fetchRecruitData,
       getNextPageParam: (lastPage, allPages) => {
