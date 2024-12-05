c5fefb5296ce8d2c327494b1dba6c1279e5c1d3c
joo93 <joo93d.o@gmail.com>
2024-12-05 13:49:57 +0900

    [etc]: 개인 깃헙 주소 추가

--- src/Components/Common/Footer.tsx
+++ src/Components/Common/Footer.tsx
@@ -5,15 +5,27 @@ export default function Footer() {
     <footer className="footer bg-neutral text-neutral-content p-10 mt-auto">
       <nav>
         <h6 className="footer-title">Frontend</h6>
-        <a className="link link-hover">고재모</a>
-        <a className="link link-hover">이승민</a>
-        <a className="link link-hover">방주영</a>
+        <a href="https://github.com/seoulmomo" className="link link-hover">
+          고재모
+        </a>
+        <a href="https://github.com/Seungmani" className="link link-hover">
+          이승민
+        </a>
+        <a href="https://github.com/joo93" className="link link-hover">
+          방주영
+        </a>
       </nav>
       <nav>
         <h6 className="footer-title">Backend</h6>
-        <a className="link link-hover">석승민</a>
-        <a className="link link-hover">김용민</a>
-        <a className="link link-hover">변상화</a>
+        <a href="https://github.com/seokseungmin" className="link link-hover">
+          석승민
+        </a>
+        <a href="https://github.com/kim975" className="link link-hover">
+          김용민
+        </a>
+        <a href="https://github.com/shbyeon20" className="link link-hover">
+          변상화
+        </a>
       </nav>
       <nav>
         <h6 className="footer-title">Social</h6>
