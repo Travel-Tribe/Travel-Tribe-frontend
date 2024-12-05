2fa7cfc61eaae02153aa3cb3c90a8ae1662b8f3c
joo93 <joo93d.o@gmail.com>
2024-12-05 12:39:56 +0900

    [refactor]: google map API 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_GOOGLE_MAPS_API_KEY: string;
+  readonly VITE_GOOGLE_MAPS_ID_WITH_LABELOFF: string;
   // 다른 환경 변수들도 여기에 추가
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
